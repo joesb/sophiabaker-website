9e903a65cb8da6f3d323e770291f701b81e9d4de
Joe Baker <joesbaker@gmail.com>
2024-07-02 18:56:04 +0100

    Fifth commit

--- src/_includes/assets/js/lightbox.js
+++ src/_includes/assets/js/lightbox.js
@@ -10,8 +10,37 @@
 $(document).ready(function () {
   $('.modal-click').on('click', function(e) {
     e.preventDefault();
-    let fig = $(this).prev('figure'); 
-    console.log(fig);
-    fig.get(0).requestFullscreen();
+    let fig = $(this).parents('.modal-click-parent');
+    if (fig.hasClass('lightbox-active')) {
+      fig.removeClass('lightbox-active');
+      document.exitFullscreen();
+    }
+    else {
+      fig.addClass('lightbox-active');
+      fig.get(0).requestFullscreen();
+    }
+    // if (fig.get(0).fullscreenElement) {
+    //   fig.removeClass('lightbox-active');
+    //   fig.get(0).exitFullScreen().catch((err) => console.error(err));
+    // }
+    // else {
+    //   fig.addClass('lightbox-active');
+    //   fig.get(0).requestFullscreen();
+    // }
   });
+
+  $(document).on('fullscreenchange', (e) => {
+    fsHandler(e);
+  });
+
 });
+
+function fsHandler(event) {
+  if (event.currentTarget.fullscreenElement == null) {
+    $('.modal-click-parent').removeClass('lightbox-active');
+  }
+}
+
+function is_fullscreen(){
+  return document.fullscreenElement != null;
+}
