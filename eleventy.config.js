a7914b1c18c739195963f211e77644556ecad678
Joe Baker <joesbaker@gmail.com>
2024-12-21 15:58:46 +0000

    Improvements to the portfolio modal

--- eleventy.config.js
+++ eleventy.config.js
@@ -182,6 +182,7 @@ export default async function(eleventyConfig) {
   });
 
   eleventyConfig.addPassthroughCopy('content/public/');
+  eleventyConfig.addWatchTarget('./src/_sass/');
 };
 
 export const config = {
