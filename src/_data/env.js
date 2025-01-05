d309279d6c0726e2d91757c9311870b0038eea1f
Joe Baker <joesbaker@gmail.com>
2025-01-05 17:05:16 +0000

    Add GA tracking

---
+++ src/_data/env.js
@@ -0,0 +1,14 @@
+import 'dotenv/config';
+const environment = process.env.ELEVENTY_ENV;
+const PROD_ENV = 'production';
+const prodUrl = 'https://sophiabaker.co.uk';
+const devUrl = 'http://localhost:8080';
+const baseUrl = environment === PROD_ENV ? prodUrl : devUrl;
+const isProd = environment === PROD_ENV;
+
+export default {
+  environment,
+  isProd,
+  baseUrl,
+  prodUrl
+};
